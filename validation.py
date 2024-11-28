40a0a44ad757500dc96c07e58f78db540dfedfc8
Dylan Boyling <dylan371@gmail.com>
2024-11-28 01:20:50 -0500

    Added logging in validation

--- validation.py
+++ validation.py
@@ -1,3 +1,7 @@
+import logging 
+
+logger = logging.getLogger()
+
 def validate_input_data(data):
     required_keys = ['id', 'numberOfChildren', 'familyComposition', 'familyUnitInPayForDecember']
     if not all(key in data for key in required_keys):
@@ -6,12 +10,15 @@ def validate_input_data(data):
     
     # TODO make single/couple constant
     if data['familyComposition'] not in ['single', 'couple']:
+        logger.info(f'familyComposition: {data['familyComposition']}')
         raise ValueError(f'Invalid family composition. Must be one single or couple.')
     
     # TODO print/log data type and its value
     if not isinstance(data['numberOfChildren'], int) or data['numberOfChildren'] < 0:
+        logger.info(f'numberOfChildren: {data['numberOfChildren']}, type: {type(data['numberOfChildren'])}')
         raise ValueError('Invalid number of children. Must be a non-negative integer.')
     
     if not isinstance(data['familyUnitInPayForDecember'], bool):
+        logger.info(f'familyUnitInPayForDecember: {data['familyUnitInPayForDecember']}, type: {type(data['familyUnitInPayForDecember'])}')
         raise ValueError('Invalid familyUnitInPayForDecember. Must be a boolean.')
 
