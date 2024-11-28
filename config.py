81309b6cee7bfa107be146f6894a8ad90e368eb6
Dylan Boyling <dylan371@gmail.com>
2024-11-27 21:14:38 -0500

    Refactored MQTT client constants into config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+HOST = 'test.mosquitto.org'
+PORT = 1883
+TOPIC_ID = '437c83fa-60bb-4c5f-b80c-bc3ea9476b19'
+INPUT_TOPIC = 'BRE/calculateWinterSupplementInput'
+OUTPUT_TOPIC = 'BRE/calculateWinterSupplementOutput'
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import paho.mqtt.client as mqtt
-
-HOST = 'test.mosquitto.org'
-PORT = 1883
-TOPIC_ID = '437c83fa-60bb-4c5f-b80c-bc3ea9476b19'
-INPUT_TOPIC = 'BRE/calculateWinterSupplementInput'
-OUTPUT_TOPIC = 'BRE/calculateWinterSupplementOutput'
+from config import *
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f'Connected with result code {reason_code}')
