e17d2aee606fe137489e65c0091ff7109fbb7165
Dylan Boyling <dylan371@gmail.com>
2024-12-01 00:52:04 -0500

    Formatting

--- tests/test_rules_engine.py
+++ tests/test_rules_engine.py
@@ -2,18 +2,21 @@ import unittest
 
 from rules_engine import calculate_base_amount, calculate_children_amount, calculate_supplement
 
+
 class TestRulesEngine(unittest.TestCase):
     def test_calculate_supplement_invalid_program(self):
         program = "invalid_program"
         number_of_children = 2
         family_composition = 'single'
         eligible = True
-        self.assertRaises(KeyError, calculate_supplement, program, number_of_children, family_composition, eligible)
+        self.assertRaises(KeyError, calculate_supplement, program,
+                          number_of_children, family_composition, eligible)
+
 
 class TestRulesEngineWinterSupplement(unittest.TestCase):
     def setUp(self):
         self.program = 'winter_supplement'
-    
+
     def test_calculate_base_amount(self):
         test_cases = [
             ('single', 60),
@@ -21,11 +24,13 @@ class TestRulesEngineWinterSupplement(unittest.TestCase):
         ]
         for family_composition, expected_base_amount in test_cases:
             with self.subTest(family_composition=family_composition):
-                    self.assertEqual(calculate_base_amount(self.program, family_composition), expected_base_amount)
+                self.assertEqual(calculate_base_amount(
+                    self.program, family_composition), expected_base_amount)
 
     def test_calculate_base_amount_invalid_composition(self):
         family_composition = 'alone'
-        self.assertRaises(KeyError, calculate_base_amount, self.program, family_composition)
+        self.assertRaises(KeyError, calculate_base_amount,
+                          self.program, family_composition)
 
     def test_calculate_children_amount(self):
         test_cases = [
@@ -35,7 +40,8 @@ class TestRulesEngineWinterSupplement(unittest.TestCase):
         ]
         for number_of_children, expected_children_amount in test_cases:
             with self.subTest(number_of_children=number_of_children):
-                    self.assertEqual(calculate_children_amount(self.program, number_of_children), expected_children_amount)
+                self.assertEqual(calculate_children_amount(
+                    self.program, number_of_children), expected_children_amount)
 
     def test_calculate_supplement_ineligible(self):
         number_of_children = 2
@@ -43,10 +49,13 @@ class TestRulesEngineWinterSupplement(unittest.TestCase):
         eligible = False
         expected_supplement_amount = 0.0
 
-        response = calculate_supplement(self.program, number_of_children, family_composition, eligible)
+        response = calculate_supplement(
+            self.program, number_of_children, family_composition, eligible)
         self.assertEqual(response['baseAmount'], expected_supplement_amount)
-        self.assertEqual(response['childrenAmount'], expected_supplement_amount)
-        self.assertEqual(response['supplementAmount'], expected_supplement_amount)
+        self.assertEqual(response['childrenAmount'],
+                         expected_supplement_amount)
+        self.assertEqual(response['supplementAmount'],
+                         expected_supplement_amount)
 
     def test_calculate_supplement(self):
         eligible = True
@@ -62,13 +71,17 @@ class TestRulesEngineWinterSupplement(unittest.TestCase):
         ]
         for number_of_children, family_composition, expected_base_amount, expected_children_amount, expected_supplement_amount in test_cases:
             with self.subTest(number_of_children=number_of_children, family_composition=family_composition, eligible=eligible):
-                    response = calculate_supplement(self.program, number_of_children, family_composition, eligible)
-                    self.assertEqual(response['baseAmount'], expected_base_amount)
-                    self.assertEqual(response['childrenAmount'], expected_children_amount)
-                    self.assertEqual(response['supplementAmount'], expected_supplement_amount)
+                response = calculate_supplement(
+                    self.program, number_of_children, family_composition, eligible)
+                self.assertEqual(response['baseAmount'], expected_base_amount)
+                self.assertEqual(
+                    response['childrenAmount'], expected_children_amount)
+                self.assertEqual(
+                    response['supplementAmount'], expected_supplement_amount)
 
     def test_calculate_supplement_invalid_composition(self):
         number_of_children = 2
         family_composition = 'alone'
         eligible = True
-        self.assertRaises(KeyError, calculate_supplement, self.program, number_of_children, family_composition, eligible)
+        self.assertRaises(KeyError, calculate_supplement, self.program,
+                          number_of_children, family_composition, eligible)
